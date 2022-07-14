2898dfe0761d44f37a413c34c1be177901769b9c
inagonov@ncc.go.jp <iurii.nagornov@aist.go.jp>
2022-07-15 05:05:02 +0900

    Add plotly and example for many psi t

--- R/example.R
+++ R/example.R
@@ -106,91 +106,85 @@ simulation_example  <-  function( verbose = TRUE , to_plot = TRUE, seed = NA,
 
 
 
-
-#' @describeIn simulation_example Function to plot whole space of parameters 
-#' related to similarity of the observation point:
+#' @describeIn simulation_example  Example of simulation for lazy start and different psi / t hyperparameters
 #'
-#' @param sim Results of the function \code{simulation_example}
-#' @param n Integer number of points for each axes to get net for plotly 3D graph
+#' @param psi_t Data.frame with different set of psi and t hyperparameters
 #'
-#' @return Plot of 3D to show the similarity of each point of the parameter space
+#' @return List of results of simulation with default values for all the parameters
 #' @export
 #'
 #' @examples
-plot_3d_similarities  <-  function( sim, r = range(0, 10), n = 12 ){
+#' NULL
+#' # it takes a time for a simulation and then it will demonstrates results, \cr
+#' # so, please, wait for a while
+#' sim = simulation_example_many_psi_t( verbose = FALSE , to_plot = FALSE )
+simulation_example_many_psi_t  <-  function( verbose = TRUE , to_plot = TRUE, seed = NA, 
+                                  model = c('Gaussian', 'linear')[2] ,
+                                  d = 2, x0 = c(3,4), probability = TRUE, 
+                                  n = 1000, r = range(0, 10),
+                                  psi_t = data.frame( psi = c( 4,  4, 8, 8,  8,  8, 10, 10 ), 
+                                                      t   = c( 8, 20, 6, 8, 16, 20,  6, 12 )  ), 
+                                  restrict_points_number = 300 ){
     
-    gen_x  =  ( 0 : (n-1) ) * ( r[2] - r[1] ) / ( n - 1 )
-    gen_y  =  gen_x
+    if ( !is.na( seed ) ) set.seed( seed = seed )
     
-    stat.obs = sim$stat.obs
-    par.sim = sim$par.sim_origin
-    stat.sim = sim$stat.sim_origin
-    x0 = sim$x0
-    iKernelABC  =  sim$spiderweb$iKernelABC
-
+    if ( verbose ) print('This code will be executed: ')
+    if ( verbose ) print( simulation_example )
     
+    # Attach packages from import list
+    packages  =  list(  randomcoloR = 'randomColor',
+                        scales = 'alpha',
+                        methods = 'new',
+                        stats = c('aggregate', 'rbinom', 'rexp', 'rnorm', 'runif', 'dist' ),
+                        utils = c('read.delim', 'read.table', 'write.table', 'globalVariables' ),
+                        grDevices = c('dev.off', 'pdf', 'rgb'),
+                        graphics = c('axis', 'legend', 'lines', 'par', 'plot', 'text', 'title', 'points' ),
+                        abc = 'abc' )
     
-    
-    # Prepare 3D plotly
-    library(plotly)
-    generate_net  <-  function( st = (0:20)/2 ){
-        # st  =  (0:20)/2
-        net  =  NULL
-        for( x in st ){
-            net_1  =  generate_points_between_two_points(n = length( st ),
-                                                         pair = data.frame( x1  =  c( x,x) , x2  = c(0,10) ) )
-            net  =  rbind( net, net_1 )
-        }
-        return( net )
+    for( pck in names( packages ) ){
+        library( package = pck, character.only = TRUE, include.only = packages[[ pck ]])
     }
     
-    net = generate_net()
-    web = sim$spiderweb
-    iKernelABC  =  web$iKernelABC
+    if ( model == 'Gaussian' ) {
+        input = Gaussian_model( d = d, x0 = x0, probability = probability, 
+                                n = n, r = r)
+    } else {
+        input  =  linear_model( d = d, x0 = x0, probability = probability, 
+                                n = n, r = r)
+    }
+    stat.sim_origin  =  input$stat.sim
+    stat.obs  =  input$stat.obs
+    par.sim_origin  =  input$par.sim
+    rm( input )
     
-    plot_web_2D( stat.sim = stat.sim, par.sim = par.sim, par.truth = data.frame( t(x0) ), 
-                 iKernelABC = web$iKernelABC, web = web, ind_X = 1, ind_Y = 2, 
-                 names = c( 'Parameter 1', 'Parameter 2' ), xlim = c(0,10), ylim = c(0,10),
-                 show_tracer = TRUE, show_obs = TRUE, show_network = TRUE, show_best = TRUE,
-                 show_u_point = TRUE, show_legend = TRUE )
+    tol = restrict_points_number / n 
     
-    points( net$x1, net$x2, pch = 20, col = alpha('grey', 0.2 ), cex = 0.8 )
+    rej = abc::abc( target = stat.obs, param = par.sim_origin, sumstat = stat.sim_origin,
+                    method = 'rejection', tol = tol )
     
-    readline( 'Press enter to continue ')
-    feature_tracers  =  get_voronoi_feature_PART_dataset( data = rbind( par.sim, net ), 
-                                                          talkative = TRUE, start_row = nrow( par.sim ) + 1 ,  
-                                                          Matrix_Voronoi = web$iKernelABC$parameters_Matrix_Voronoi) 
-    # iKernelABC$parameters_Matrix_Voronoi )
+    stat.sim  =  stat.sim_origin[ rej$region, ]
+    par.sim   =   par.sim_origin[ rej$region, ]    
     
-    sim_tracers  =  iKernel_point_dataset( Matrix_iKernel = feature_tracers$M_iKernel, 
-                                           t = iKernelABC$t, nr = nrow( feature_tracers$M_iKernel ), 
-                                           iFeature_point = iKernelABC$kernel_mean_embedding )
+    webnet  =  list( )
     
-    # plot_ly net with similarity:
-    net$sim  =  sim_tracers
-    x1 = unique( net$x1 )
-    x2 = unique( net$x2 )
-    m = matrix( data = net$sim, ncol = length(x1), nrow = length(x2) )
-    
-    p2 <- plot_ly(x = x1, y = x2, z = m ) %>% add_surface(
-        contours = list(
-            z = list(
-                show=TRUE,
-                usecolormap=TRUE,
-                highlightcolor="#ff0000",
-                project = list( z = TRUE )
-            )
-        )
-    ) %>% add_trace( type = "scatter3d", mode="line", # markers
-                     x = c(x0[1],x0[1]), y = c(x0[2], x0[2]), z = c(0, max(net$sim)) ) 
-    
+    for( j in 1:nrow( psi_t ) ){
+        psi =  psi_t$psi[ j ]
+        t   =  psi_t$t[ j ]
+        web  =  spiderweb( psi = psi, t = t, param = par.sim_origin, 
+                           stat.sim = stat.sim_origin, stat.obs = stat.obs, 
+                           talkative = TRUE, check_pos_def = FALSE ,
+                           n_bullets = 5, n_best = 20, halfwidth = 0.5, 
+                           epsilon = 0.001 )
+        webnet[[ j ]]  =  web
+    }
 
-    print( p2 )
     
-    
-    
-    
-    
-    return( NULL )
+    return( list( stat.sim_origin  =  stat.sim_origin, 
+                  par.sim_origin  =  par.sim_origin,
+                  x0 = x0,
+                  stat.obs  =  stat.obs, 
+                  stat.sim  =  stat.sim,
+                  par.sim   =  par.sim,
+                  psi_t     =  psi_t,
+                  webnet  = webnet ) )
 }
-
---
+++ R/plotly.R
@@ -0,0 +1,193 @@
+
+
+#' @describeIn simulation_example Function to plot whole space of parameters 
+#' related to similarity of the observation point:
+#'
+#' @param sim Results of the function \code{simulation_example}
+#' @param n Integer number of points for each axes to get net for plotly 3D graph
+#'
+#' @return Plot of 3D to show the similarity of each point of the parameter space
+#' @export
+#'
+#' @examples
+plot_3d_similarities  <-  function( sim, r = range(0, 10), n = 12 ){
+    
+    gen_x  =  ( 0 : (n-1) ) * ( r[2] - r[1] ) / ( n - 1 )
+    gen_y  =  gen_x
+    
+    stat.obs = sim$stat.obs
+    par.sim = sim$par.sim_origin
+    stat.sim = sim$stat.sim_origin
+    x0 = sim$x0
+    iKernelABC  =  sim$spiderweb$iKernelABC
+    
+    
+    
+    
+    # Prepare 3D plotly
+    library(plotly)
+    generate_net  <-  function( st = (0:20)/2 ){
+        # st  =  (0:20)/2
+        net  =  NULL
+        for( x in st ){
+            net_1  =  generate_points_between_two_points(n = length( st ),
+                                                         pair = data.frame( x1  =  c( x,x) , x2  = c(0,10) ) )
+            net  =  rbind( net, net_1 )
+        }
+        return( net )
+    }
+    
+    net = generate_net()
+    web = sim$spiderweb
+    iKernelABC  =  web$iKernelABC
+    
+    plot_web_2D( stat.sim = stat.sim, par.sim = par.sim, par.truth = data.frame( t(x0) ), 
+                 iKernelABC = web$iKernelABC, web = web, ind_X = 1, ind_Y = 2, 
+                 names = c( 'Parameter 1', 'Parameter 2' ), xlim = c(0,10), ylim = c(0,10),
+                 show_tracer = TRUE, show_obs = TRUE, show_network = TRUE, show_best = TRUE,
+                 show_u_point = TRUE, show_legend = TRUE )
+    
+    points( net$x1, net$x2, pch = 20, col = alpha('grey', 0.2 ), cex = 0.8 )
+    
+    readline( 'Press enter to continue ')
+    feature_tracers  =  get_voronoi_feature_PART_dataset( data = rbind( par.sim, net ), 
+                                                          talkative = TRUE, start_row = nrow( par.sim ) + 1 ,  
+                                                          Matrix_Voronoi = web$iKernelABC$parameters_Matrix_Voronoi) 
+    # iKernelABC$parameters_Matrix_Voronoi )
+    
+    sim_tracers  =  iKernel_point_dataset( Matrix_iKernel = feature_tracers$M_iKernel, 
+                                           t = iKernelABC$t, nr = nrow( feature_tracers$M_iKernel ), 
+                                           iFeature_point = iKernelABC$kernel_mean_embedding )
+    
+    # plot_ly net with similarity:
+    net$sim  =  sim_tracers
+    x1 = unique( net$x1 )
+    x2 = unique( net$x2 )
+    m = matrix( data = net$sim, ncol = length(x1), nrow = length(x2) )
+    
+    p2 <- plot_ly(x = x1, y = x2, z = m ) %>% add_surface(
+        contours = list(
+            z = list(
+                show=TRUE,
+                usecolormap=TRUE,
+                highlightcolor="#ff0000",
+                project = list( z = TRUE )
+            )
+        )
+    ) %>% add_trace( type = "scatter3d", mode="line", # markers
+                     x = c(x0[1],x0[1]), y = c(x0[2], x0[2]), z = c(0, max(net$sim)) ) 
+    
+    
+    print( p2 )
+    
+    
+    
+    
+    
+    return( NULL )
+}
+
+
+
+
+#' @describeIn simulation_example Function to plot whole space of parameters 
+#' related to similarity of the observation point for each set of hyperparameters psi and t 
+#' 
+#' @param simnet Results of the function \code{simulation_example_many_psi_t}
+#' 
+#' @return Many 3D plots to show the similarity of each point of the parameter space
+#' for each set of hyperparameters psi and t 
+#' 
+#' @export
+#'
+#' @examples
+#' NULL
+plot_3d_net_similarities  <-  function( simnet, r = range(0, 10), n = 12 ){
+    
+    gen_x  =  ( 0 : (n-1) ) * ( r[2] - r[1] ) / ( n - 1 )
+    gen_y  =  gen_x
+    
+    stat.obs = simnet$stat.obs 
+    par.sim = simnet$par.sim_origin
+    stat.sim = simnet$stat.sim_origin
+    x0 = simnet$x0
+    # iKernelABC  =  sim$spiderweb$iKernelABC
+    psi_t  =  simnet$psi_t
+    
+    
+    
+    # Prepare 3D plotly
+    # library(plotly)
+    generate_net  <-  function( st = (0:20)/2 ){
+        # st  =  (0:20)/2
+        net  =  NULL
+        for( x in st ){
+            net_1  =  generate_points_between_two_points(n = length( st ),
+                                                         pair = data.frame( x1  =  c( x,x) , x2  = c(0,10) ) )
+            net  =  rbind( net, net_1 )
+        }
+        return( net )
+    }
+    
+    for( j in 1:length( simnet$webnet)  ){
+        
+        net = generate_net()
+        web  =  simnet$webnet[[ j ]] 
+        iKernelABC  =  simnet$webnet[[ j ]]$iKernelABC
+        Xbest  =  as.numeric( simnet$webnet[[ j ]]$par.best )
+        
+        if ( FALSE ){
+            iKernelABC  =  web$iKernelABC
+            
+            plot_web_2D( stat.sim = stat.sim, par.sim = par.sim, par.truth = data.frame( t(x0) ), 
+                         iKernelABC = web$iKernelABC, web = web, ind_X = 1, ind_Y = 2, 
+                         names = c( 'Parameter 1', 'Parameter 2' ), xlim = c(0,10), ylim = c(0,10),
+                         show_tracer = TRUE, show_obs = TRUE, show_network = TRUE, show_best = TRUE,
+                         show_u_point = TRUE, show_legend = TRUE )
+        
+            points( net$x1, net$x2, pch = 20, col = alpha('grey', 0.2 ), cex = 0.8 )
+        
+            readline( 'Press enter to continue ')
+        }
+        
+        feature_tracers  =  get_voronoi_feature_PART_dataset( data = rbind( par.sim, net ), 
+                                                              talkative = TRUE, start_row = nrow( par.sim ) + 1 ,  
+                                                              Matrix_Voronoi = web$iKernelABC$parameters_Matrix_Voronoi) 
+        # iKernelABC$parameters_Matrix_Voronoi )
+        
+        sim_tracers  =  iKernel_point_dataset( Matrix_iKernel = feature_tracers$M_iKernel, 
+                                               t = iKernelABC$t, nr = nrow( feature_tracers$M_iKernel ), 
+                                               iFeature_point = iKernelABC$kernel_mean_embedding )
+        
+        # plot_ly net with similarity:
+        net$sim  =  sim_tracers
+        x1 = unique( net$x1 )
+        x2 = unique( net$x2 )
+        m = matrix( data = net$sim, ncol = length(x1), nrow = length(x2) )
+        
+        p2 <- plot_ly(x = x1, y = x2, z = m ) %>% add_surface(
+            contours = list(
+                z = list(
+                    show=TRUE,
+                    usecolormap=TRUE,
+                    highlightcolor="#ff0000",
+                    project = list( z = TRUE )
+                )
+            )
+        ) %>% add_trace( type = "scatter3d", mode="line", # markers
+                           x = c( Xbest[1], Xbest[1] ), 
+                           y = c( Xbest[2], Xbest[2] ), 
+                           z = c(0, max(net$sim) )
+        ) %>% layout(title = paste('Psi = ', psi_t$psi[j], ',  t = ', psi_t$t[j] ) 
+        ) %>% add_trace( type = "scatter3d", mode="line", # markers
+                        x = c(x0[1],x0[1]), y = c(x0[2], x0[2]), z = c(0, max(net$sim))  ) 
+        
+        
+        print( p2 )
+        
+        readline( 'Press enter to continue ')
+    }
+
+    return( NULL )
+}
+
