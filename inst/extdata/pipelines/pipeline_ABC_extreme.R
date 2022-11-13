29f80c98ab8b5e682b729916ecdd5f5ac73b120b
inagonov@ncc.go.jp <iurii.nagornov@aist.go.jp>
2022-11-14 03:18:06 +0900

    Update pipeline_ABC_extreme

--- inst/extdata/pipelines/pipeline_ABC_extreme.R
+++ inst/extdata/pipelines/pipeline_ABC_extreme.R
@@ -40,10 +40,10 @@ if ( FALSE ){
 ###      dimension and stochastic term.
 
 file_name   =  'output.txt'
-models      =  c( 'Gaussian', 'Linear' )
-dimensions  =  (1:20)*2
-stochastic_terms   =   c( 0, 0.1, 0.3, 0.7, 1, 1.5 )
-rng  =  c( 0, 10 )   # range of parameters
+# models      =  c( 'Gaussian', 'Linear' )
+dimension  =  20
+stochastic_term   =   0 
+rng  =  c( 0, 2000 )   # range of parameters
 restrict_points_number  =  300
 
 #  By default number of processors in parallel calculations
@@ -56,92 +56,84 @@ if ( file.exists( file_name) ) unlink( file_name )
 
 
 x0     =  c(10, 50, 90, 130, 180, 280, 390, 430, 520, 630, 1010, 1050, 1090, 1130, 1180, 1280, 1390, 1430, 1520, 1630)
-sigma  =  x0 / 5
-x = 1:20000 / 10 
+sigma  =  rep( 100, length( x0 ) )
 A = 250
-f <- function( A, x, x0, sigma ){
-    Gaus  =  function( A, x, x0, sigma ) A * exp( -( x - x0 ) ** 2 / sigma )
-    y = sum( unlist( lapply( 1:length( x0 ), FUN = function( i ) Gaus( A = A, x = x,
-                                        x0 = x0[ i ], sigma  =  sigma[ i ]  )  )  )  )
-    return( y )
-}
+
+if ( FALSE ){
+    x = 1:20000 / 10 
+    A = 250
+    f <- function( A, x, x0, sigma ){
+        Gaus  =  function( A, x, x0, sigma ) A * exp( -( x - x0 ) ** 2 / sigma )
+        y = sum( unlist( lapply( 1:length( x0 ), FUN = function( i ) Gaus( A = A, x = x,
+                                            x0 = x0[ i ], sigma  =  sigma[ i ]  )  )  )  )
+        return( y )
+    }
 
 
 y = sapply( 1:length( x ), FUN = function( i ) f( A = A, x = x[ i ], x0 = x0, sigma = sigma ) )
 
 plot( x, y, type = 'l', xlim = c( 0, 200 ) )
-
+}
 #### ___________________________________________________________________
 
 DF = NULL   # Data frame to collect results of all the simulations
-for( model in models ){
-    for( dimension in dimensions ){
-        for( stochastic_term in stochastic_terms ){
-            
-            input  =  NULL
-            x0  =  runif( n = dimension, min = rng[1], max = rng[2] )
-            Number_of_points  =  max( c( 50 * dimension, restrict_points_number ) )
-            
-            if ( model == 'Gaussian' ) {
-                input = Gaussian_model( d = dimension, x0 = x0, probability = TRUE, 
-                                        n = Number_of_points, r = rng,
-                                        noise = stochastic_term )
-            }
-            if ( model == 'Linear' ) {
-                input  =  linear_model( d = dimension, x0 = x0, probability = TRUE, 
-                                        n = Number_of_points, r = rng,
-                                        noise = stochastic_term )
-            }
-            
-            if ( is.null( input ) ) stop( 'Model name is incorrect' )
-            stat.sim_origin  =  input$stat.sim
-            stat.obs  =  input$stat.obs
-            par.sim_origin  =  input$par.sim
-            rm( input )
-            
-            # Apply restrict number of points:
-            tol = restrict_points_number / nrow( stat.sim_origin )
-            rej = abc::abc( target = stat.obs, param = par.sim_origin, sumstat = stat.sim_origin,
-                            method = 'rejection', tol = tol )
-            
-            stat.sim  =  stat.sim_origin[ rej$region, ]
-            par.sim   =   par.sim_origin[ rej$region, ] 
-            
-            psi_t  =  adjust_psi_t( par.sim = par.sim, stat.sim = stat.sim, 
-                                    stat.obs = stat.obs, talkative = FALSE, 
-                                    check_pos_def = FALSE, 
-                                    n_best = 8, cores = 4 )
-            
-            ikern  =  iKernelABC( psi = psi_t$psi[1], t = psi_t$t[1], 
-                                  param = par.sim, 
-                                  stat.sim = stat.sim, 
-                                  stat.obs = stat.obs, 
-                                  talkative = FALSE, 
-                                  check_pos_def = FALSE )
-            
-            G = matrix( data = ikern$similarity, ncol = 1 )
-            DF_new  =  Get_call_all_methods(    
-                model_name = model, 
-                dimension  = dimension,
-                stochastic_term = stochastic_term, 
-                iterations  =  1:12,
-                stat.obs = stat.obs, 
-                stat.sim = stat.sim, 
-                par.sim  = par.sim, 
-                G        = G, 
-                par.truth  =  x0, 
-                cores = cores )
-            DF  =  rbind( DF, DF_new )
-            
-            if ( file.exists( file_name ) ){
-                write.table(file = file_name, x = DF_new , append = TRUE, sep = '\t', 
-                            row.names = FALSE, col.names = FALSE )
-            } else {
-                write.table(file = file_name, x = DF_new , append = TRUE, sep = '\t', 
-                            row.names = FALSE, col.names = TRUE )
-            }
-        }
-    }
+
+input  =  NULL
+
+Number_of_points  =  max( c( 50 * dimension, restrict_points_number ) )
+
+
+input = Gaussian_model( d = dimension, x0 = x0, probability = FALSE, 
+                        n = Number_of_points, r = rng, A = A,
+                        noise = stochastic_term )
+
+
+if ( is.null( input ) ) stop( 'Model name is incorrect' )
+stat.sim_origin  =  input$stat.sim
+stat.obs  =  input$stat.obs
+par.sim_origin  =  input$par.sim
+rm( input )
+
+# Apply restrict number of points:
+tol = restrict_points_number / nrow( stat.sim_origin )
+rej = abc::abc( target = stat.obs, param = par.sim_origin, sumstat = stat.sim_origin,
+                method = 'rejection', tol = tol )
+
+stat.sim  =  stat.sim_origin[ rej$region, ]
+par.sim   =   par.sim_origin[ rej$region, ] 
+
+psi_t  =  adjust_psi_t( par.sim = par.sim, stat.sim = stat.sim, 
+                        stat.obs = stat.obs, talkative = FALSE, 
+                        check_pos_def = FALSE, 
+                        n_best = 8, cores = 4 )
+
+ikern  =  iKernelABC( psi = psi_t$psi[1], t = psi_t$t[1], 
+                      param = par.sim, 
+                      stat.sim = stat.sim, 
+                      stat.obs = stat.obs, 
+                      talkative = FALSE, 
+                      check_pos_def = FALSE )
+
+G = matrix( data = ikern$similarity, ncol = 1 )
+DF_new  =  Get_call_all_methods(    
+    model_name = model, 
+    dimension  = dimension,
+    stochastic_term = stochastic_term, 
+    iterations  =  1:12,
+    stat.obs = stat.obs, 
+    stat.sim = stat.sim, 
+    par.sim  = par.sim, 
+    G        = G, 
+    par.truth  =  x0, 
+    cores = cores )
+DF  =  rbind( DF, DF_new )
+
+if ( file.exists( file_name ) ){
+    write.table(file = file_name, x = DF_new , append = TRUE, sep = '\t', 
+                row.names = FALSE, col.names = FALSE )
+} else {
+    write.table(file = file_name, x = DF_new , append = TRUE, sep = '\t', 
+                row.names = FALSE, col.names = TRUE )
 }
 
 ###  Results of simulation is collected in dataset DF
