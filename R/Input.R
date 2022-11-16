4c0bb957e8247ae571cb9955a43b409002ab942a
inagonov@ncc.go.jp <iurii.nagornov@aist.go.jp>
2022-11-17 04:26:19 +0900

    Add sigma to Gaussian model

--- R/Input.R
+++ R/Input.R
@@ -144,15 +144,16 @@ linear_model  <-  function( d = 1, x0 = 3, probability = TRUE, noise = 0.2,
 #' @examples
 #' NULL
 model  <-  function( name = c( 'Gaussian', 'Linear' )[1], 
-                     parameter, x0, stat.obs, noise = 0 ){
-    
+                     parameter, x0, stat.obs, noise = 0, sigma = 1 ){
+
     d = length( x0 )
+    if ( length( sigma ) < d )  sigma = rep( sigma, d )
     sim = as.data.frame( matrix( data = 0, nrow = 1, ncol = d ) ) 
     names( sim )  =  names( stat.obs )
     
     if ( name == 'Gaussian' ){
         for( i in 1:d ){
-            sim[ 1, i ]  = exp( x = - ( parameter[1 , i ] - x0[ i ] ) ** 2 / 2 ) + noise * runif(1)
+            sim[ 1, i ]  = exp( x = - ( parameter[1 , i ] - x0[ i ] ) ** 2 / 2 / sigma[d] ) + noise * runif(1)
         }
     } else {
         if ( name == 'Linear' ){
