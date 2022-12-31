bd6315e04618c01dd7488d516d4eac1202a4d232
inagonov@ncc.go.jp <iurii.nagornov@aist.go.jp>
2022-12-31 22:57:44 +0900

    Add function get_dataset_of_Linear_function

--- R/Input.R
+++ R/Input.R
@@ -1,9 +1,5 @@
 
-
-# MODELS ------------------------------------------------------------------
-
-
-#' The model of simulations that is based on Gaussian functions for each dimension 
+#' Function to get dataset of simulations that is based on Gaussian functions for each dimension 
 #'
 #' @description The function \code{get_dataset_of_Gaussian_model()} allows to generate 
 #' parameters and statistics of simulations that are based on Gaussian function for each dimension: \cr
@@ -19,7 +15,7 @@
 #' @param A Vector of exponent multiplication factors
 #' @param sigma Vector of sigmas in Gaussian function. Length of vector should be equal d.
 #'
-#' @return The function \code{get_dataset_of_Gaussian_model()} returns list of two objects: \cr
+#' @return The function \code{get_dataset_of_Gaussian_model()} returns list of three objects: \cr
 #' - stat.sim - data frame of simulations statistics,
 #' - par.sim - data frame of parameters,
 #' - stat.obs - data frame of an observation point.
@@ -98,6 +94,73 @@ Gauss_function  <-  function( d = 1, x0 = 3, r = range(0,10), noise = 0,
 }
 
 
+
+#' Function to get dataset of simulations that is based on Linear functions for each dimension 
+#'
+#' @description The function \code{get_dataset_of_Linear_model()} allows to generate 
+#' parameters and statistics of simulations that are based on Linear function for each dimension: \cr
+#' \code{f( x ) = { A1 * x1 / x01, ..., An * xn / x0n  } = { y1, y2, ..., yn } } is a vector of output data
+#' 
+#'
+#' @param d Dimension of the parameter and model space
+#' @param x0 Numeric vector with non-zero values. The vector's length should be equal d.  
+#' \code{x0} contents the truth value of parameter. Each number in the vector should be within the range r
+#' @param probability Logical, if TRUE then apply uneven distribution for parameters generation
+#' @param n Integer number of points in data frame
+#' @param r Range \code{r = c(min, max)}, by default  \code{r = range(0,10)} for all the dimensions
+#' @param A Vector of multiplication factors.
+#'
+#' @return The function \code{get_dataset_of_Linear_model()} returns list of three objects: \cr
+#' - stat.sim - data frame of simulations statistics,
+#' - par.sim - data frame of parameters,
+#' - stat.obs - data frame of an observation point.
+#' 
+#' @export
+#' 
+#' @examples
+#' NULL
+get_dataset_of_Linear_model  <-  function( d = 1, x0 = 3, r = range(0,10), 
+                                             noise = 0, A = 1, 
+                                             probability = TRUE, n = 1000 ) {
+    
+    # Define and generate the parameters:
+    par.sim  =  data.frame( matrix( NA, nrow = n, ncol = d ) )
+    names( par.sim )  =  paste0( 'X', 1:d )
+    for( i in 1:d ){
+        rnd  =  runif( 10 * n , min = r[1], max = r[2] )
+        if ( probability ) { 
+            prob  =  lapply( rnd, FUN = function( x ){ 
+                ( A[ i ] - Gauss_function( d = 1, x0 = x0[ i ], 
+                                           r = r, noise = noise, A = A[ i ], sigma = (r[2]-r[1])/5, 
+                                           par.sim1 = as.data.frame(x) ) ) / A[ i ]
+            } )
+            prob  =  unlist( prob )
+        } else {
+            prob  =  rep( 1, length( rnd ))
+        }
+        par.sim[ , i ]  =  sample( x = rnd, size = n, 
+                                   replace = FALSE, prob = prob )
+    }
+    
+    stat.sim  =  data.frame( matrix( NA, nrow = n, ncol = d ) )
+    names( stat.sim )  =  paste0( 'Y', 1:d )
+    for( i in 1:d ){
+        stat.sim[ , i ]  = unlist( lapply( X = par.sim[ , i ], FUN = function( x ){ 
+            Linear_function( d = 1, x0 = x0[ i ], 
+                            r = r, noise = noise, A = A[ i ], 
+                            par.sim1 = x )
+        } )   )
+        # A * exp( x = - ( par.sim[ , i ] - x0[ i ] ) ** 2 / 2 ) + 
+        # runif( n = n, min = -0.5, max = 0.5 ) * noise
+    }
+    
+    stat.obs  =  data.frame( NULL )
+    for ( i in 1:d ){
+        stat.obs[ 1, paste0( 'Y', i ) ]  =  A[ i ]
+    }
+    
+    return( list( stat.sim = stat.sim, par.sim = par.sim, stat.obs = stat.obs )  )
+}
 
 
 #' @describeIn get_dataset_of_Linear_model Function to get output of Linear function for d dimension case
