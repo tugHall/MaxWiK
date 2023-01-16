4559fa41d19791b56adcce1cc2149f3daaebfabc
inagonov@ncc.go.jp <iurii.nagornov@aist.go.jp>
2023-01-17 02:46:13 +0900

    First version of pipeline_sampler.R

--- inst/extdata/pipelines/pipeline_samplers.R
+++ inst/extdata/pipelines/pipeline_samplers.R
@@ -2,6 +2,7 @@
 ###  Pipeline to get parameter estimation based on sampling schemes
 
 library('MaxWiK')
+library('parallel')
 
 # DEfine the working folder:
 wd  =  '../Simulation'
@@ -33,7 +34,7 @@ model       =  c( 'Gaussian', 'Linear' )[ 1 ]
 dimension   =  2
 stochastic_term   =   c( 0, 0.1, 0.5, 1, 2 )[ 1 ]
 rng  =  c( 0, 1000 )   # range of parameters
-restrict_points_number  =  500
+restrict_points_number  =  Number_of_points  =  500
 d = max( dimension )
 A      =  ( ( 1:d ) + 12 ) * 100  # Amplitude for Gauss function / Linear function
 sigma  =  rep( rng[2]/ 5, d )     # Sigma for Gauss function 
@@ -54,16 +55,17 @@ cores = 4
 if ( file.exists( file_name) ) unlink( file_name )
 
 
-Get_data  =  function( dimension, rng, restrict_points_number, 
+Get_data  =  function( dimension, rng, 
                        Number_of_points, model,
-                       A, sigma, stochastic_term ){
+                       A, sigma, stochastic_term, 
+                       probability  =  TRUE ){
     
     input  =  NULL
     x0  =  runif( n = dimension, min = rng[1], max = rng[2] )
-    Number_of_points  =  max( c( 50 * dimension, restrict_points_number ) )
+    Number_of_points  =  max( c( 50 * dimension, Number_of_points ) )
     
     if ( model == 'Gaussian' ) {
-        input = get_dataset_of_Gaussian_model( d = dimension, x0 = x0, probability = TRUE, 
+        input = get_dataset_of_Gaussian_model( d = dimension, x0 = x0, probability = probability, 
                                                n = Number_of_points, r = rng, 
                                                A = A[1:dimension], sigma = sigma[1:dimension], 
                                                noise = stochastic_term )
@@ -73,7 +75,7 @@ Get_data  =  function( dimension, rng, restrict_points_number,
                          noise = stochastic_term  ) 
     }
     if ( model == 'Linear' ) {
-        input  =  get_dataset_of_Linear_model( d = dimension, x0 = x0, probability = TRUE, 
+        input  =  get_dataset_of_Linear_model( d = dimension, x0 = x0, probability = probability, 
                                                n = Number_of_points, r = rng, A = A[1:dimension],
                                                noise = stochastic_term )
         model_par = list( d = dimension, x0 = x0, r = rng, 
@@ -82,18 +84,10 @@ Get_data  =  function( dimension, rng, restrict_points_number,
     }
     
     if ( is.null( input ) ) stop( 'Model name is incorrect' )
-    stat.sim_origin  =  input$stat.sim
+    stat.sim  =  input$stat.sim
     stat.obs  =  input$stat.obs
-    par.sim_origin  =  input$par.sim
+    par.sim   =  input$par.sim
     rm( input )
-    
-    # Apply restrict number of points:
-    tol = restrict_points_number / nrow( stat.sim_origin )
-    rej = abc::abc( target = stat.obs, param = par.sim_origin, sumstat = stat.sim_origin,
-                    method = 'rejection', tol = tol )
-    
-    stat.sim  =  stat.sim_origin[ rej$region, ]
-    par.sim   =   par.sim_origin[ rej$region, ] 
     
     return( list( stat.obs  =  stat.obs,
                   stat.sim  =  stat.sim, 
@@ -103,25 +97,59 @@ Get_data  =  function( dimension, rng, restrict_points_number,
 
 
 input  =  Get_data( dimension = dimension, rng = rng, 
-                    restrict_points_number = restrict_points_number, 
                     Number_of_points = Number_of_points,
                     model = 'Gaussian',
-                    A = A, sigma = sigma, stochastic_term = 0 )
+                    A = A, sigma = sigma, 
+                    stochastic_term = stochastic_term, 
+                    probability = TRUE )
 par.sim   =  input$par.sim 
 stat.sim  =  input$stat.sim
 stat.obs  =  input$stat.obs
 par.truth =  input$model_par$x0
 model_par =  input$model_par
 
-stat.sim_init =  stat.sim
-par.sim_init  =  par.sim
-
-hyper  =  Get_hyperparameters(stat.obs = stat.obs, stat.sim = stat.sim, 
+hyper    =  Get_hyperparameters(stat.obs = stat.obs, stat.sim = stat.sim, 
                               par.sim = par.sim, par.truth = par.truth )
+
 utils::capture.output( hyper, file = 'HyperParameters.txt', append = FALSE )
 
 psi_t  =  hyper$iKernel$psi_t
 
+# Maximal number of iteration in sampling for each method
+nmax  =  1000
+
+if ( model  == 'Gaussian' ){
+    
+    input  =  get_dataset_of_Gaussian_model( d = dimension, 
+                                             x0 = model_par$x0, 
+                                             r = model_par$r, 
+                                             noise = model_par$noise, 
+                                             A = model_par$A, 
+                                             sigma = model_par$sigma, 
+                                             probability = FALSE, 
+                                             n = nmax )
+}
+
+par.sim   =  rbind( par.sim,  input$par.sim )
+stat.sim  =  rbind( stat.sim, input$stat.sim )
+rm( input )
+
+
+rej      =  abc( target = stat.obs, param = par.sim, 
+                 sumstat = stat.sim, tol = hyper$tolerance, method = 'rejection' ) 
+
+w        =  which( rej$region )
+w        =  w[ which( w > ( nrow( par.sim ) - nmax ) ) ]
+
+rm( rej )
+
+if ( length( w ) == 0 ) stop( 'There is NO points closed to observation one!' )
+
+
+data_par_est  =  list( init  =  par.sim[ 1 : ( nrow( par.sim) - nmax ), ], w = w )
+data_MSE      =  data.frame( w = w )
+
+############### Start of a method
 
 Meth_Kern  =  data.frame( Method = c('K2-ABC', 'K2-ABC', 'K2-ABC', 'Rejection', 
                                      'Loclinear', 'Neuralnet', 'Ridge',
@@ -131,44 +159,66 @@ Meth_Kern  =  data.frame( Method = c('K2-ABC', 'K2-ABC', 'K2-ABC', 'Rejection',
                                      'iKernel', 'iKernel') 
 )
 
-
-method_name  =  Meth_Kern$Method[ 4 ]
-kernel_name  =  Meth_Kern$Kernel[ 4 ]
-
-stat.sim_itt =  stat.sim
-par.sim_itt  =  par.sim
-
-# Maximal number of itteration in sampling for each method
-nmax  =  100   
-
-res = NULL
-for( itt in 1:nmax ){
+# Start of looop for all the methods:
+for( j in 1:length( Meth_Kern$Method ) ){
     
-    input  =  restrict_data( par.sim = par.sim_itt, 
-                             stat.sim = stat.sim_itt, 
-                             stat.obs = stat.obs, 
-                             size = restrict_points_number ) 
-    stat.sim_itt =  input$stat.sim
-    par.sim_itt  =  input$par.sim
+    method_name  =  Meth_Kern$Method[ j ]
+    kernel_name  =  Meth_Kern$Kernel[ j ]
     
-    new_par = Get_parameter( method_name = method_name, 
-                             kernel_name = kernel_name, 
-                             stat.obs   =  stat.obs, 
-                             stat.sim   =  stat.sim_itt, 
-                             par.sim    =  par.sim_itt, 
-                             G          =  NULL, 
-                             hyper      =  hyper )
+    MK  =  paste0( method_name, '_', kernel_name )
     
-    res = rbind( res, new_par )
+    print( MK )
+    pb <- txtProgressBar(min = 0, max = max( w ), style = 3)
+    # DF  =  NULL
+    # time_start  =  Sys.time( )
+    res  =  lapply( X = w, FUN = function( x ){
     
-    add_arg  =  list( par.sim1 =  as.data.frame( new_par ) )
-
-    new_sim  =  do.call( what = model_function, 
-                         args = c( model_par, add_arg ) )
+        G = NULL
+        if ( kernel_name  ==  'iKernel' ){
+            ikern  =  iKernelABC( psi = psi_t$psi[1], t = psi_t$t[1], 
+                                  param = par.sim[1:x, ], 
+                                  stat.sim = stat.sim[1:x, ], 
+                                  stat.obs = stat.obs, 
+                                  talkative = FALSE, 
+                                  check_pos_def = FALSE )
+            
+            G = matrix( data = ikern$similarity, ncol = 1 )
+        }
+        new_par = Get_parameter( method_name = method_name, 
+                                 kernel_name = kernel_name, 
+                                 stat.obs   =  stat.obs, 
+                                 stat.sim   =  stat.sim[1:x, ], 
+                                 par.sim    =  par.sim[1:x, ], 
+                                 G          =  NULL, 
+                                 hyper      =  hyper )
+        setTxtProgressBar(pb, x)
+        return( new_par )
+        
+    } )
+    # running_time  =  as.numeric( difftime(Sys.time(), time_start, units = "secs")[[1]] )
     
-    par.sim_itt  =  rbind( par.sim_itt, new_par )
-    stat.sim_itt =  rbind( stat.sim_itt, new_sim )
-}
+    data_par_est[[ MK ]]  =  as.data.frame( do.call( rbind, res ) ) 
+    
+    for( i in 1:nrow( data_par_est[[ MK ]] ) ){      
+        
+        new_par  =  data_par_est[[ MK ]][ i, ]
+        model_par_all  =  c( model_par, list( par.sim1 = new_par ) )
+        model_par_all$noise  =  0 
+        sim_est  =  do.call( model_function, model_par_all )
+        
+        MSE  =  MSE_sim(stat.obs = stat.obs, stat.sim = sim_est ) 
+    
+        data_MSE[ i, MK ]  = MSE
+    }
+    
+}   # End of loop for all the methods
+
+
+
+
+plot(x = w, y = log( res_2 ), type = 'p' )
+points( x = w, y = log( res_1 ), col = 'blue' )    
+    
 
 
 
@@ -188,25 +238,6 @@ for( itt in 1:nmax ){
 
 
 
-
-# Define model function:
-if ( model_name == 'Gaussian' ) {
-    model  =  model
-    arg0 = list(  name = c( 'Gaussian', 'Linear' )[1],
-                  x0 = par.truth, 
-                  stat.obs = stat.obs, 
-                  noise = stochastic_term )
-} else {
-    model  =  model
-    arg0 = list(  name = c( 'Gaussian', 'Linear' )[2],
-                  x0 = par.truth, 
-                  stat.obs = stat.obs, 
-                  noise = stochastic_term )
-} 
-
-### Plot for 2D example:
-plot_2D( x = par.sim$x1, y = par.sim$x2, xr = c(0,10), yr = c( 0,10) )
-points( x = par.truth$X1, y = par.truth$X2, col = 'red', pch = 18, cex = 2.5 )
 
 
 
@@ -227,9 +258,6 @@ DF_1_S  =  mclapply( 1:nrow(Meth_Kern) , FUN = function( mk ){
                     size       =  ifelse( Number_of_points > 500, 500 , Number_of_points )  
     )
 }, mc.cores  =  cores )
-
-
-
 
 # Check an error
 bad   =  sapply( DF_1_S, inherits, what = "try-error" )
