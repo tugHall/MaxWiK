e2250918ba4649f22eed1c6b668e887043d4f9f6
inagonov@ncc.go.jp <iurii.nagornov@aist.go.jp>
2024-07-08 21:06:17 +0900

    Update manual

--- R/Sampler_iKernel.R
+++ R/Sampler_iKernel.R
@@ -2,8 +2,6 @@
 
 #' Function to generate parameters and simulate a model based on MaxWiK algorithm 
 #'
-#' @param psi Integer number. Size of each Voronoi diagram or number of areas/points in the Voronoi diagrams
-#' @param t Integer number of trees in the Isolation Forest
 #' @param stat.sim Summary statistics of the simulations (model output)
 #' @param stat.obs Summary statistics of the observation point
 #' @param par.sim Data frame of parameters of the model
